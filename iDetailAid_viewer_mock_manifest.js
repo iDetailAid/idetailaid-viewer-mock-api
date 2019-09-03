3b82acdca499ebf02eedd13ea1a7ba5fe2dd0dd8
idetailaid-team <46049843+idetailaid-team@users.noreply.github.com>
2019-09-03 15:39:12 +0100

    Create iDetailAid_viewer_mock_manifest.js

---
+++ iDetailAid_viewer_mock_manifest.js
@@ -0,0 +1,127 @@
+window.uk = window.uk || {};
+window.uk.co = window.uk.co || {};
+window.uk.co.idetailaid = window.uk.co.idetailaid || {};
+
+window.uk.co.idetailaid.MockManifest = {
+  "id": "default",
+  "title": "Mock Presentation (Diabotan / Global)",
+  "presentation": {
+    "title": "Mock Presentation",
+    "hidden": 0,
+    "meta": {},
+    "id": 201,
+    "country_code": "go"
+  },
+  "product": {
+    "id": 1,
+    "name": "Diabotan"
+  },
+  "company": {
+    "id": 1,
+    "name": "PharmaCo"
+  },
+  "css": [],
+  "js": [],
+  "templates": {
+    "1336": {
+      "id": 1336,
+      "title": "Mock Core Flow Template",
+      "file": "07feb1219520756f8f3236668cec01de.html",
+      "reference_ids": [],
+      "navigation_ids": []
+    },
+    "1347": {
+      "id": 1347,
+      "title": "Mock Sub Flow Template",
+      "file": "79b16d11f43874b15da438b3a136e911.html",
+      "reference_ids": [],
+      "navigation_ids": []
+    }
+  },
+  "references": [],
+  "navigation": [],
+  "matrix": [
+    [
+      {
+        "title": "Mock Slide One",
+        "file": "9560d5eeaaabed937b0a35e6fe4cc148.html",
+        "image": "9560d5eeaaabed937b0a35e6fe4cc148.png",
+        "mandatory": 0,
+        "reference_ids": [],
+        "meta": {},
+        "template_id": null,
+        "overlays": [],
+        "category": "Safety",
+        "id": 1348
+      }
+    ],
+    [
+      {
+        "title": "Mock Slide Two",
+        "file": "bfc25cc9623a1790d0f49fe79adbddd0.html",
+        "image": "bfc25cc9623a1790d0f49fe79adbddd0.png",
+        "mandatory": 0,
+        "reference_ids": [],
+        "meta": {},
+        "template_id": null,
+        "overlays": [],
+        "category": null,
+        "id": 1349
+      },
+      {
+        "title": "Mock Slide Four",
+        "file": "373becea7a7512d50ab5f2e26c5900cd.html",
+        "image": "373becea7a7512d50ab5f2e26c5900cd.png",
+        "mandatory": 0,
+        "reference_ids": [],
+        "meta": {},
+        "template_id": 1336,
+        "overlays": [
+          {
+            "id": "53d362eb-3b40-46ad-94bf-0cb943294a70",
+            "title": "Overlay 1",
+            "style": "diabotan-overlay-large",
+            "image": "373becea7a7512d50ab5f2e26c5900cd_overlay_53d362eb-3b40-46ad-94bf-0cb943294a70_large.png",
+            "image_url": "http://192.168.10.100/assets/pharmaco/diabotan/go/373becea7a7512d50ab5f2e26c5900cd_overlay_53d362eb-3b40-46ad-94bf-0cb943294a70_large.png",
+            "in_use": true
+          }
+        ],
+        "category": null,
+        "id": 1344
+      }
+    ],
+    [
+      {
+        "title": "Mock Slide Three",
+        "file": "65733e4f037dfc13b0f840246fa79c33.html",
+        "image": "65733e4f037dfc13b0f840246fa79c33.png",
+        "mandatory": 0,
+        "reference_ids": [],
+        "meta": {},
+        "template_id": 1347,
+        "overlays": [
+          {
+            "id": "fd63d504-d032-420e-eb59-f3eaa8e773f8",
+            "title": "Overlay 1",
+            "style": "diabotan-overlay-large",
+            "image": "65733e4f037dfc13b0f840246fa79c33_overlay_fd63d504-d032-420e-eb59-f3eaa8e773f8_large.png",
+            "image_url": "http://192.168.10.100/assets/pharmaco/diabotan/go/65733e4f037dfc13b0f840246fa79c33_overlay_fd63d504-d032-420e-eb59-f3eaa8e773f8_large.png",
+            "in_use": true
+          },
+          {
+            "id": "0d299d66-0591-41fa-9df3-4997c539c2f1",
+            "title": "Overlay 2",
+            "style": "diabotan-overlay-large",
+            "image": "65733e4f037dfc13b0f840246fa79c33_overlay_0d299d66-0591-41fa-9df3-4997c539c2f1_large.png",
+            "image_url": "http://192.168.10.100/assets/pharmaco/diabotan/go/65733e4f037dfc13b0f840246fa79c33_overlay_0d299d66-0591-41fa-9df3-4997c539c2f1_large.png",
+            "in_use": false
+          }
+        ],
+        "category": "Efficacy",
+        "id": 1346
+      }
+    ]
+  ],
+  "timeStamp": 1567462155648,
+  "settings": {}
+}
